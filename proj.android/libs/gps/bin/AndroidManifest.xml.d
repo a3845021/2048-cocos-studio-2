e1657caaebbe01ad640a8cc63694150d6101e376
Huu NC <huunguyenchi91@gmail.com>
2016-07-04 13:24:57 +0700

    add undo, gg analytics, admob

--- proj.android/libs/gps/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/Users/applehouse/Documents/CocosProjects/2048Clone/proj.android/libs/gps/bin/AndroidManifest.xml : \
-/Users/applehouse/Documents/CocosProjects/2048Clone/proj.android/libs/gps/AndroidManifest.xml \
