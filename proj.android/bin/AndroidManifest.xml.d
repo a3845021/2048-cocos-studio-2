4e59171f0b12a9105da9cd7129b1a2f66349ea81
Huu NC <huunguyenchi91@gmail.com>
2016-07-04 13:15:56 +0700

    add undo, gg analytics, admob

--- proj.android/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/Users/applehouse/Documents/CocosProjects/2048Clone/proj.android/bin/AndroidManifest.xml : \
-/Users/applehouse/Documents/CocosProjects/2048Clone/proj.android/AndroidManifest.xml \
--- proj.android/gen/com/somestudio/open2048/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.somestudio.open2048;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = false;
-}
